ee232a250dacdd6f2e02f950ffec63612c23f12e
akhlei <angusleikh@gmail.com>
2016-07-20 21:33:57 +0000

    admin/users controller now checks for admin category for current_user, redirects if not an admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,10 @@
 class Admin::UsersController < ApplicationController
   def index
-    @users = User.all
+    if current_user.admin?
+      @users = User.all
+      render :index
+    else
+      redirect_to root_path, notice: "Get out! You must be an admin to view that page!"
+    end
   end
 end
