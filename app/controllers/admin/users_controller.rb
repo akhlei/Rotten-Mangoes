244fdac9737225d773f5a0c7e8fa44f80a234df9
akhlei <angusleikh@gmail.com>
2016-07-21 02:18:47 +0000

    added the necessary pathing and admin protection if/else statements with redirects

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -16,4 +16,53 @@ class Admin::UsersController < ApplicationController
       not_admin_redirect
     end
   end
+
+  def new
+    if current_user.admin?
+      @user = User.new
+    else
+      not_admin_redirect
+    end
+  end
+
+  def edit
+    if current_user.admin? 
+      @user = User.find(params[:id])
+    else
+      not_admin_redirect
+    end
+  end
+
+  # @user is a new movie passing in the parameters from this create page
+  # if it properly saves, go to the all movies page, otherwise go to the 'new' page again
+  def create
+    if current_user.admin?
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to admin_users_path, notice: "User with email: #{@user.email} was successfully created!"
+      else
+        render :new
+      end
+    else
+      not_admin_redirect
+    end
+  end 
+
+  def destroy
+    if current_user.admin?
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to admin_users_path
+    else
+      not_admin_redirect
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(
+      :email, :firstname, :lastname, :password, :password_confirmation, :admin
+    )
+  end
 end
