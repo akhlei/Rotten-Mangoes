09d6e65cec56e58f7b0a87dc560cb89e49ad0b2d
akhlei <angusleikh@gmail.com>
2016-07-22 20:05:56 +0000

    renamed for session paths and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   get 'reviews/new'
   get 'reviews/create'
 
-  get 'sessions/new'
-  get 'sessions/create'
-
   get 'users/new'
   get 'users/create'
 
@@ -14,7 +11,7 @@ Rails.application.routes.draw do
   end
   
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :session, only: [:new, :create, :destroy]
 
   namespace :admin do 
     resources :users
